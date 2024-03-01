a8d17ee92acd90e0bdf64230021b524de16bd672
DidenkoSveta <garpia.lana@gmail.com>
2024-03-02 00:10:31 +0700

    galery portfolio

--- app/js/modules/portfolio.js
+++ app/js/modules/portfolio.js
@@ -1,35 +1,130 @@
-// Функции для открытия и закрытия модальных окон
-function openModal(modalId) {
-   var modal = document.getElementById(modalId);
-   modal.style.display = "block";
- }
+document.addEventListener('DOMContentLoaded', () => {
+   const openModal = (modal) => {
+     modal.style.display = 'block';
+     // Инициализация галереи при открытии модального окна
+     const gallerySlides = slides[modal.id];
+     if (gallerySlides) {
+       slideIndex[modal.id] = 0;
+       const img = modal.querySelector('.gallery-slide img');
+       if (img) img.src = gallerySlides[slideIndex[modal.id]];
+     }
+   };
  
- function closeModal(modalId) {
-   var modal = document.getElementById(modalId);
-   modal.style.display = "none";
- }
+   const closeModal = (modal) => {
+     modal.style.display = 'none';
+   };
  
- // Обработчики событий для карточек портфолио
- document.querySelectorAll('.portfolio__card').forEach((card, index) => {
-   card.addEventListener('click', function(event) {
-     event.preventDefault(); // Предотвращаем действие по умолчанию
-     var modalId = "modal" + card.querySelector('.portfolio__caption').textContent.replace(/\s+/g, '');
-     openModal(modalId);
+   const cards = document.querySelectorAll('.portfolio__card');
+   cards.forEach(card => {
+     card.addEventListener('click', (event) => {
+       event.preventDefault();
+       const modalId = card.getAttribute('href').substring(1);
+       const modal = document.getElementById(modalId);
+       if (modal) {
+         openModal(modal);
+       } else {
+         console.error('Modal with ID ' + modalId + ' not found');
+       }
+     });
    });
- });
  
- // Обработчики событий для кнопок закрытия модальных окон
- document.querySelectorAll('.modal .close').forEach(closeButton => {
-   closeButton.addEventListener('click', function() {
-     var modal = this.closest('.modal');
-     closeModal(modal.id);
+   const closeButtons = document.querySelectorAll('.modal .close');
+   closeButtons.forEach(button => {
+     button.addEventListener('click', () => {
+       const modal = button.closest('.modal');
+       closeModal(modal);
+     });
    });
- });
  
- // Закрытие модального окна при клике вне его содержимого
- window.addEventListener('click', function(event) {
-   if (event.target.className === 'modal') {
-     closeModal(event.target.id);
-   }
+   window.addEventListener('click', (event) => {
+     if (event.target.classList.contains('modal')) {
+       closeModal(event.target);
+     }
+   });
+ 
+   // Инициализация индекса текущего изображения для каждого модального окна
+   const slideIndex = {};
+ 
+   // Массив путей к изображениям для каждой пары
+   const slides = {
+     'modalGeorgiyEllina': [
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-001.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-002.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-003.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-004.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-005.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-006.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-007.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-008.jpg",
+       "images/img/portfilio-slider/GeorgiyEllina/GeorgiyEllina-009.jpg",
+     ],
+     'modalRuslanVera': [
+      "images/img/portfilio-slider/RuslanVera/RuslanVera-001.jpg",
+      "images/img/portfilio-slider/RuslanVera/RuslanVera-002.jpg",
+      "images/img/portfilio-slider/RuslanVera/RuslanVera-003.jpg",
+      "images/img/portfilio-slider/RuslanVera/RuslanVera-004.jpg",
+      "images/img/portfilio-slider/RuslanVera/RuslanVera-005.jpg",
+      "images/img/portfilio-slider/RuslanVera/RuslanVera-006.jpg",
+      "images/img/portfilio-slider/RuslanVera/RuslanVera-007.jpg",
+    ],
+    'modalYuraDayana': [
+      "images/img/portfilio-slider/YuraDayana/YuraDayana-001.jpg",
+      "images/img/portfilio-slider/YuraDayana/YuraDayana-002.jpg",
+      "images/img/portfilio-slider/YuraDayana/YuraDayana-003.jpg",
+      "images/img/portfilio-slider/YuraDayana/YuraDayana-004.jpg",
+      "images/img/portfilio-slider/YuraDayana/YuraDayana-005.jpg",
+      "images/img/portfilio-slider/YuraDayana/YuraDayana-006.jpg",
+    ],
+    'modalDmitriiAnastasia': [
+      "images/img/portfilio-slider/DmitriiAnastasia/DmitriiAnastasia-001.jpg",
+      "images/img/portfilio-slider/DmitriiAnastasia/DmitriiAnastasia-002.jpg",
+      "images/img/portfilio-slider/DmitriiAnastasia/DmitriiAnastasia-003.jpg",
+      "images/img/portfilio-slider/DmitriiAnastasia/DmitriiAnastasia-004.jpg",
+      "images/img/portfilio-slider/DmitriiAnastasia/DmitriiAnastasia-005.jpg",
+    ],
+    'modalKinnTaya': [
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-001.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-002.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-003.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-004.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-005.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-006.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-007.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-008.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-009.jpg",
+      "images/img/portfilio-slider/KinnTaya/KinnTaya-010.jpg",
+    ],
+
+    'modalNikolayIrina': [
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-001.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-002.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-003.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-004.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-005.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-006.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-007.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-008.jpg",
+      "images/img/portfilio-slider/NikolayIrina/NikolayIrina-009.jpg",
+    ],
+   };
+   
+   // Функция для переключения слайдов
+   window.changeSlide = (modalId, n) => {
+     const gallerySlides = slides[modalId];
+     if (gallerySlides) {
+       slideIndex[modalId] = (slideIndex[modalId] + n + gallerySlides.length) % gallerySlides.length;
+       const img = document.getElementById(modalId).querySelector('.gallery-slide img');
+       if (img) img.src = gallerySlides[slideIndex[modalId]];
+     }
+   };
+ 
+   // Обработчики событий для кнопок навигации
+   document.querySelectorAll('.prev, .next').forEach(button => {
+     button.addEventListener('click', () => {
+       const modalId = button.closest('.modal').id;
+       const n = button.classList.contains('prev') ? -1 : 1;
+       changeSlide(modalId, n);
+     });
+   });
  });
  
